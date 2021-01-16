0f25fc04732e2351bac3f5840cd7b6107048cfd7
bosstnp <boss.32560@gmail.com>
2021-01-16 21:15:02 +0700

    Add canvas and draw a grid

--- client/index.js
+++ client/index.js
@@ -1,1 +1,34 @@
-alert("Hello, World!")
+const canvas = document.getElementById("canvas");
+const g = canvas.getContext("2d");
+
+const WIDTH = canvas.width;
+const HEIGHT = canvas.height;
+const SIZE = 50;
+
+const interval = setInterval(() => {
+
+    g.fillStyle = "white";
+    g.fillRect(0, 0, WIDTH, HEIGHT);
+    
+    update();
+
+    for (let i = 1; i <= 6; i++) {
+        g.strokeStyle = "black";
+        g.lineWidth = 1;
+
+        g.beginPath();
+        g.moveTo(i * SIZE, 0);
+        g.lineTo(i * SIZE, HEIGHT);
+        g.stroke();
+        
+        g.beginPath();
+        g.moveTo(0, i * SIZE);
+        g.lineTo(WIDTH, i * SIZE);
+        g.stroke();
+    }
+
+}, 1000 / 60);
+
+function update() {
+
+}
